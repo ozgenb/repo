704ceead75bd318bb656abb72e2c22c55a50edec
0zgen <45134101+0zgen@users.noreply.github.com>
2019-02-09 22:47:59 +0100

    Fade in logo omg <3

--- gf/versterking/brein.js
+++ gf/versterking/brein.js
@@ -6,6 +6,11 @@ if ($(window).width() > 600) {
 	}
 }
 
+function fade() {
+$('#header-boven').delay(100).animate({'opacity':'1'},600);
+}
+
+
 function openMenu() {
 	var hdrMnu = document.getElementById("headerMenu");
 	if (hdrMnu.className === "header-menu") {
